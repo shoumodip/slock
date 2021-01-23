9d5a4aeb3aebc7392a03dbfdc62f47f107e16cea
shoumodip <shoumodipkar@gmail.com>
2021-01-23 17:44:05 +0530

    Dark mode

--- config.h
+++ config.h
@@ -3,10 +3,10 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT]      = "#121212", /* After initialization */
-	[INPUT]     = "#3a3a3a", /* During input */
-	[FAILED]    = "#d68787", /* Wrong password */
-	[CAPS]      = "#d8af5f", /* CapsLock on */
+	[INIT]      = "#002b36", /* After initialization */
+	[INPUT]     = "#073642", /* During input */
+	[FAILED]    = "#dc322f", /* Wrong password */
+	[CAPS]      = "#b58900", /* CapsLock on */
 };
 
 /* Treat a cleared input like a wrong password (color) */
