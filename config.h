7b92cd2986ffacc471a0e415efa5bd68a9e12d0f
shoumodip <shoumodipkar@gmail.com>
2021-01-27 12:13:20 +0530

    Changed colors

--- config.h
+++ config.h
@@ -3,10 +3,10 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT]      = "#002b36", /* After initialization */
-	[INPUT]     = "#073642", /* During input */
-	[FAILED]    = "#dc322f", /* Wrong password */
-	[CAPS]      = "#b58900", /* CapsLock on */
+	[INIT]      = "#161616", /* After initialization */
+	[INPUT]     = "#282828", /* During input */
+	[FAILED]    = "#cc241d", /* Wrong password */
+	[CAPS]      = "#d79921", /* CapsLock on */
 };
 
 /* Treat a cleared input like a wrong password (color) */
