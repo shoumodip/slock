9cd8758d0608daaadb1e01c8502c75af8132b426
shoumodip <shoumodipkar@gmail.com>
2021-03-10 16:16:47 +0530

    Switched to gruvbox

--- config.h
+++ config.h
@@ -3,10 +3,10 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT]      = "#2b2b2b", /* After initialization */
-	[INPUT]     = "#323232", /* During input */
-	[FAILED]    = "#bc3f3c", /* Wrong password */
-	[CAPS]      = "#6a8759", /* CapsLock on */
+	[INIT]      = "#282828", /* After initialization */
+	[INPUT]     = "#3c3836", /* During input */
+	[FAILED]    = "#9d0006", /* Wrong password */
+	[CAPS]      = "#689d6a", /* CapsLock on */
 };
 
 /* Treat a cleared input like a wrong password (color) */
